4f15b06001bedaa5121c8eb93dce2cac14321272
anitrama <81196286+anitrama@users.noreply.github.com>
2021-05-23 21:08:20 -0700

    Add files via upload

---
+++ airr_to_obsidian_format.py
@@ -0,0 +1,30 @@
+# This script will be used to convert Airr quote formatting to Obsidian formatting
+# Written by: anitrama
+
+import re
+from pathlib import Path
+
+user_input = input('paste file path here: ')
+input_fixed = user_input.strip('"')
+filename = Path(input_fixed)
+
+final_list = []
+with open(filename, 'r') as f:
+    lines = f.readlines()
+    non_blank_lines = []
+    for line in lines:
+        if line.rstrip():
+            non_blank_lines.append(line)
+
+    for line in non_blank_lines:
+        new_string = re.findall('\[[^\]]*\]|\([^\)]*\)|\"[^\"]*\"|\S+', line)
+        quote_words = new_string[2:]
+        quote = ' '.join(quote_words)
+        if quote == '':
+            quote = "1"
+        final_form = str('- [' + quote + "]" + new_string[1])
+        final_list.append(final_form)
+
+with open(filename, 'w') as f:
+    for line in final_list:
+        f.write('%s\n' % line)
